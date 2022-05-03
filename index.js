e027b40a5d6a36b2e5d464a9c441530307fad10b
Naftali Kulik <nkulik1994@gmail.com>
2022-05-02 20:40:03 -0400

    commit tree 88bd6b6f41abecb50b0f24bf5b04a23ddcd30d19

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const userInfo = {
+        name: userName,
+        email: userEmail
+    }
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userInfo)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const newUser = document.createElement('h2')
+        newUser.textContent = data.id
+        document.body.appendChild(newUser)
+    })
+    .catch(error => {
+        const errorMessage = document.createElement('h2')
+        errorMessage.textContent = error.message
+        document.body.appendChild(errorMessage)
+    })
+}
